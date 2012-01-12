0d8351d17a94bb80d5f2bd4cce6bea0522ed250c
Bhavani Iyer <bhavani@apache.org>
2012-01-12 18:10:06 +0000

    UIMA-2333 changes to the build source script to include linux and windows build artifacts.
    git-svn-id: https://svn.apache.org/repos/asf/uima/uimacpp/trunk@1230677 13f79535-47bb-0310-9956-ffa450edef68

--- buildSrcTree.sh
+++ buildSrcTree.sh
@@ -18,7 +18,7 @@
 #   under the License.
 
 # for now, change versionnumber in line below
-srcName=uimacpp-2.4.0
+srcName=uimacpp-2.4.0-src
 
 if [ "$1" = 'TRACE' ]
     then set -vx ;shift
@@ -67,7 +67,24 @@ else
   LIBEXT=so
 fi
 
+
+# No Configure script means we have to run autogen
+if [ -x "./configure" ] ; then
+        # even if we have a configure script, if the template has been updated we should
+        # run it again to account for the changes, which also means we need to run a autogen
+        # to create the actual script and associated autoconf artifacts.
+    if [ "./configure.ac" -ot "./configure" ]  ; then
+             echo "configure is up to date"
+    else
+            ./autogen.sh
+    fi
+else
+  ./autogen.sh
+fi
+
+
 mkdir $target_dir
+mkdir $target_dir/m4
 mkdir $target_dir/data
 mkdir $target_dir/docs
 mkdir $target_dir/src
@@ -88,6 +105,17 @@ cp $CPL  $UIMACPP_SOURCE/RELEASE_NOTES* $target_dir/
 cp $CPL  $UIMACPP_SOURCE/README.4src $target_dir/README
 cp $CPL  $UIMACPP_SOURCE/README.4sdk $target_dir/
 cp $CPL  $UIMACPP_SOURCE/NOTICE.4sdk $target_dir/
+cp $CPL  $UIMACPP_SOURCE/configure $target_dir/
+cp $CPLR $UIMACPP_SOURCE/config $target_dir
+cp $CPL  $UIMACPP_SOURCE/Makefile.in $target_dir
+cp $CPL  $UIMACPP_SOURCE/configure.ac $target_dir
+cp $CPL  $UIMACPP_SOURCE/autogen.sh $target_dir
+cp $CPL  $UIMACPP_SOURCE/Makefile.am $target_dir
+cp $CPL  $UIMACPP_SOURCE/m4/find_icu.m4  $target_dir/m4
+cp $CPL  $UIMACPP_SOURCE/m4/find_xerces.m4 $target_dir/m4
+cp $CPL  $UIMACPP_SOURCE/m4/find_apr.m4 $target_dir/m4
+cp $CPL  $UIMACPP_SOURCE/m4/find_activemq.m4 $target_dir/m4
+cp $CPL  $UIMACPP_SOURCE/aclocal.m4  $target_dir
 
 print -u2 "copying from $UIMACPP_SOURCE/data"
 cp $CPL  $UIMACPP_SOURCE/data/*.xsd $target_dir/data/
@@ -100,17 +128,8 @@ cp $CPL $UIMACPP_SOURCE/docs/doxyheader.html $target_dir/docs
 cp $CPL $UIMACPP_SOURCE/docs/overview_and_setup.html $target_dir/docs
 
 print -u2 "copying from $UIMACPP_SOURCE/src"
-cp $CPL $UIMACPP_SOURCE/src/Makefile.in $target_dir/src/
-cp $CP $UIMACPP_SOURCE/src/aclocal.m4 $target_dir/src/
-cp $CP $UIMACPP_SOURCE/src/install-sh $target_dir/src/
-cp $CP $UIMACPP_SOURCE/src/config.guess $target_dir/src/
-cp $CP $UIMACPP_SOURCE/src/config.sub $target_dir/src/
-cp $CP $UIMACPP_SOURCE/src/ltmain.sh $target_dir/src/
-cp $CPL $UIMACPP_SOURCE/src/Makefile.unix $target_dir/src/
-cp $CPL $UIMACPP_SOURCE/src/configure $target_dir/src/
 cp $CPL $UIMACPP_SOURCE/src/base.mak $target_dir/src/
-
-cp $CPL $UIMACPP_SOURCE/src/configure.in $target_dir/src/
+cp $CPL $UIMACPP_SOURCE/src/Makefile.in $target_dir/src/
 cp $CPL $UIMACPP_SOURCE/src/Makefile.am $target_dir/src/
 
 print -u2 "copying from $UIMACPP_SOURCE/src/cas"
@@ -152,12 +171,29 @@ cp $CPL $UIMACPP_SOURCE/src/test/fvtTestfile $target_dir/src/test/
 find $target_dir/src/test -type f -iname '*.l?' -exec rm -f {} \;
 find $target_dir/src/test -type f -iname '*.o' -exec rm -f {} \;
 
+print -u2 "copying files required for a Windows build"
+cp $CPL $UIMACPP_SOURCE/buildsdk.cmd $target_dir
+cp $CPL $UIMACPP_SOURCE/docs/builddocs.cmd $target_dir/docs
+cp $CPL $UIMACPP_SOURCE/src/*.cmd $target_dir/src
+cp $CPL $UIMACPP_SOURCE/examples/*.cmd $target_dir/examples
+cp $CPL $UIMACPP_SOURCE/src/*.bat $target_dir/src
+cp $CPL $UIMACPP_SOURCE/src/test/*.bat $target_dir/src/test
+cp $CPL $UIMACPP_SOURCE/src/*.vcproj $target_dir/src
+cp $CPL $UIMACPP_SOURCE/src/test/src/*.vcproj $target_dir/src/test/src
+cp $CPL $UIMACPP_SOURCE/src/test/fvtTestfile.debug $target_dir/src/test
+cp $CPL $UIMACPP_SOURCE/examples/src/*.vcproj $target_dir/examples/src
+cp $CPL $UIMACPP_SOURCE/examples/tutorial/src/*.vcproj $target_dir/examples/tutorial\src
+cp $CPL $UIMACPP_SOURCE/src/*.sln $target_dir/src
+cp $CPL $UIMACPP_SOURCE/src/test/*.sln $target_dir/src/test
+cp $CLP $UIMACPP_SOURCE/examples/src/*.sln $target_dir/examples/src
+
+
 print -u2 "removing unused files from $UIMACPP_SOURCE"
 find $target_dir -depth -type d -name .svn -exec rm -rf {} \;
-find $target_dir -type f -iname '*.sln' -exec rm -f {} \;
-find $target_dir -type f -iname '*.vcproj' -exec rm -f {} \;
-find $target_dir -type f -iname '*.cmd' -exec rm -f {} \;
-find $target_dir -type f -iname '*.bat' -exec rm -f {} \;
+#find $target_dir -type f -iname '*.sln' -exec rm -f {} \;
+#find $target_dir -type f -iname '*.vcproj' -exec rm -f {} \;
+#find $target_dir -type f -iname '*.cmd' -exec rm -f {} \;
+#find $target_dir -type f -iname '*.bat' -exec rm -f {} \;
 find $target_dir -type f -iname '*~' -exec rm -f {} \;
 
 print -u2 "DONE ... Source image created in $target_dir"
