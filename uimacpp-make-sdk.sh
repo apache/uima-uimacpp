d6530313c5d599655cb57889ededa7267803ac02
Burn Lewis <burnlewis@gmail.com>
2020-03-04 10:35:01 -0500

    [UIMA-6190] Run the JNI test on OSX

--- uimacpp-make-sdk.sh
+++ uimacpp-make-sdk.sh
@@ -43,14 +43,14 @@ mkdir -p "$TARGET"
 
 # Linux / Mac OSX customizations
 # If ActiveMQ falsely believes OpenSSL is installed use: AMQARG=--disable-ssl
-# or if it is installed elsewhere use (e.g. on OSX):     AMQARG=--with-openssl=/usr/local/opt/openssl
+# On Mac OpenSSL is usually installed in /usr/local/opt/openssl
 
 UNAME=`uname -s`
 if [ "$UNAME" = "Darwin" ]; then
 	LIBEXT=dylib
 	INCDIR=darwin
 	ICUARG=MacOSX
-	AMQARG=
+	AMQARG=--with-openssl=/usr/local/opt/openssl
 else
 	LIBEXT=so
 	INCDIR=linux
@@ -143,7 +143,7 @@ if [ ! -f "${APR}.tar.gz" ]; then
 fi
 if [ ! -f "$APR/build/apr_rules.out" ]; then
     cd $APR
-	echo Building APR
+    echo Building APR
     ./configure --prefix="$PREFIX"
     make install
     cd ..
@@ -154,36 +154,32 @@ fi
 if [ -z $TESTONLY ]; then
     # Build APR-UTIL
     if [ ! -f "${APRUTIL}.tar.gz" ]; then
-	wget http://mirrors.ocf.berkeley.edu/apache/apr/${APRUTIL}.tar.gz
-	tar -xf ${APRUTIL}.tar.gz
+        wget http://mirrors.ocf.berkeley.edu/apache/apr/${APRUTIL}.tar.gz
+        tar -xf ${APRUTIL}.tar.gz
     fi
     if [ ! -f ${APRUTIL}/apu-config.out ]; then
-	cd $APRUTIL
-	echo Building APR-UTIL
-	./configure --prefix=$PREFIX --with-apr=$PREFIX/bin/apr-1-config
-	make install
-	cd ..
+        cd $APRUTIL
+        echo Building APR-UTIL
+        ./configure --prefix=$PREFIX --with-apr=$PREFIX/bin/apr-1-config
+        make install
+        cd ..
     else
-	echo "APR-UTIL previously built"
+        echo "APR-UTIL previously built"
     fi
 
     # Build ActiveMQ-C++
     if [ ! -f ${AMQCPP}-src.tar.gz ]; then
-	wget http://archive.apache.org/dist/activemq/activemq-cpp/${AMQVERSION}/${AMQCPP}-src.tar.gz
-	tar -xf ${AMQCPP}-src.tar.gz
+        wget http://archive.apache.org/dist/activemq/activemq-cpp/${AMQVERSION}/${AMQCPP}-src.tar.gz
+        tar -xf ${AMQCPP}-src.tar.gz
     fi
     if [ ! -f ${AMQCPP}/src/main/.libs/libactivemq-cpp.${LIBEXT} ]; then
-	cd $AMQCPP
-	echo Building ActiveMQ
-        if [ "$UNAME" = "Darwin" ]; then
-           LDFLAGS="-L/usr/local/opt/openssl@1.1/lib" CPPFLAGS="-I/usr/local/opt/openssl@1.1/include" ./configure $AMQARG --prefix=$PREFIX --with-apr=$PREFIX/bin/apr-1-config
-        else
-           ./configure $AMQARG --prefix=$PREFIX --with-apr=$PREFIX/bin/apr-1-config
-        fi
-	make install
-	cd ..
+        cd $AMQCPP
+        echo Building ActiveMQ
+        ./configure $AMQARG --prefix=$PREFIX --with-apr=$PREFIX/bin/apr-1-config
+        make install
+        cd ..
     else
-	echo "ActiveMQ C++" previously built
+        echo "ActiveMQ C++" previously built
     fi
 fi
 
