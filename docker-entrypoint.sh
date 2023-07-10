5391072d9b0f818f0c544625c08e504a3b9e68bb
Pablo Duboue <pablo.duboue@gmail.com>
2023-07-10 18:13:24 -0400

    Docker built version

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+# check to see if this file is being run or sourced from another script
+_is_sourced() {
+	# https://unix.stackexchange.com/a/215279
+	[ "${#FUNCNAME[@]}" -ge 2 ] \
+		&& [ "${FUNCNAME[0]}" = '_is_sourced' ] \
+		&& [ "${FUNCNAME[1]}" = 'source' ]
+}
+
+_main() {
+    /usr/local/uimacpp/bin/runAECpp "$@"
+}
